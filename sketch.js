e35ccbd6ba7221a341bb2f6accd97fdbf002b7b1
Narren Singh <71593009+nsp10@users.noreply.github.com>
2020-11-13 19:45:29 +0530

    dev faster

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,9 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(60,200,100,PI/2);
 
     bird = new Bird(100,100);
 
+    newChain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -58,7 +61,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    newChain.display();
     platform.display();
 }
